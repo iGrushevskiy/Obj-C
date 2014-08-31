13d91a618600397ceb7a65f1dbe6efa98a7f87b1
Igor Grushevskiy <grushevskiy.igor@gmail.com>
2014-08-31 17:52:01 +0300

    Update big_code_file.m

--- big_code_file.m
+++ big_code_file.m
@@ -5,9 +5,22 @@ void (^myFirstBlock)(NSString *) = ^(NSString *str){
 myFirstBlock(@"Hello");
 myFirstBlock(@"World");
 
+
+
 void (^logCount)(NSArray *) = ^(NSArray *array){
   NSLog(@"There are %lu objects in this array", [array count]);
 };
 
 logCount(@[@"Mr.", @"Higgie"]);
 logCount(@[@"Mr.", @"Jony", @"Ive", @"Higgie"]);
+
+
+NSArray *newHats = @[@"Cowboy", @"Conductor", @"Baseball", 
+  @"Beanie", @"Beret", @"Fez"];
+
+void (^enumBlock)(NSString *, NSUInteger, BOOL *)=
+  ^(NSString *hat, NSUInteger index, BOOL *stop){
+  NSLog(@"Trying on hat %@", hat);
+};
+
+[newHats enumerateObjectsUsingBlock:enumBlock];
