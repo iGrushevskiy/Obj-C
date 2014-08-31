649faec0a0d4d574127dc901b6c88cc8f9fd4bf4
Igor Grushevskiy <grushevskiy.igor@gmail.com>
2014-08-31 17:16:00 +0300

    Update for_enum_nsdictionary.m
    Added one more NSDictionary

--- for_enum_nsdictionary.m
+++ for_enum_nsdictionary.m
@@ -8,3 +8,21 @@ for (NSString *word in funnyWords) {
   NSString *definition = funnyWords[word];
   NSLog(@"%@ is defined as %@", word, definition);
 }
+
+NSDictionary *newHats = @{                                                                                                    
+  @"Cowboy": @"White",
+  @"Conductor": @"Brown",
+  @"Baseball": @"Red"
+};
+
+for (NSString *hat in newHats){
+
+  NSString *color = newHats[hat];
+  NSLog(@"Trying on new %@ %@ hat", color, hat);
+
+  if([mrHiggie tryOnHat:hat withColor:color]) {
+    NSLog(@"Mr. Higgie loves it");
+  } else {
+    NSLog(@"Mr. Higgie hates it");
+  }
+}
