2e89991ec53ed32747bff8d6bc7574286aa67ab1
Igor Grushevskiy <grushevskiy.igor@gmail.com>
2014-08-31 16:02:26 +0300

    Create for_enum_nsdictionary.m

---
+++ for_enum_nsdictionary.m
@@ -0,0 +1,10 @@
+NSDictionary *funnyWords = @{
+  @"Schadenfreude": @"pleasure derived by someone from another person's misfortune.",
+  @"Portmanteau": @"consisting of or combining two or more separable aspects or qualities",
+  @"Penultimate": @"second to the last"
+};
+
+for (NSString *word in funnyWords) {
+  NSString *definition = funnyWords[word];
+  NSLog(@"%@ is defined as %@", word, definition);
+}
